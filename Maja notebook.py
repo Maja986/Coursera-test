30b50f78f625c0e54e342753adcaf6c72501e6fd
Maja986 <120668164+Maja986@users.noreply.github.com>
2022-12-27 13:01:53 +0100

    Test1

---
+++ Maja notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # H1 My Jupyter Notebook on IBM Watson Studio
+
+# **Maja Šerić**
+# Data Manager
+
+# *I am interested in data science because I wanna progress in my career*
+
+# ### H3 The code below greets the user
+
+# In[1]:
+
+
+usertext = input("What is your name? ")
+print("Hello", usertext)
+
+
+# [YouTube](https://youtube.com)
+# ![image](https://images-platform.99static.com/_xqufX_4sCqkAwJXuWqcxYkseKs=/1304x145:2015x856/500x500/top/smart/99designs-contests-attachments/102/102050/attachment_102050302)
+# ~~Scratch this.~~
+
+# In[ ]:
+
+
+
+
